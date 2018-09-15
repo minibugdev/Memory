242f76706957fb78e12fafc278a42680a8dcf552
minibugdev <minibugdev@gmail.com>
2018-09-15 07:00:18 +0700

    Add schedule and map

--- src/app/App.kt
+++ src/app/App.kt
@@ -7,8 +7,7 @@ import kotlinext.js.jsObject
 import kotlinx.css.*
 import kotlinx.css.properties.LineHeight
 import react.*
-import react.dom.div
-import react.dom.img
+import react.dom.*
 import styled.*
 
 @JsModule("src/app/images/cover.jpg")
@@ -67,7 +66,6 @@ class App : RComponent<AppProps, AppState>() {
 
         val config: AxiosConfigSettings = jsObject { url = "https://res.cloudinary.com/ting-pop/image/list/wedding.json" }
         axios<ResourceResponse>(config).then { response ->
-            console.log(response.data)
             setState {
                 galleryImagePublicIds = response.data.resources
                         .map { it.publicId }
@@ -173,13 +171,119 @@ class App : RComponent<AppProps, AppState>() {
 
             styledDiv {
                 css {
+                    backgroundColor = Color.white
+                    margin(0.px)
+                    padding(80.px, 0.px)
+                }
+
+                styledH1 {
+                    +"SCHEDULE"
+                }
+
+                div {
+                    +"On the 15th of September, 2018"
+                }
+                br {}
+
+                styledTable {
+                    css {
+                        margin(LinearDimension.auto)
+                    }
+                    tbody {
+                        tr {
+                            styledTd {
+                                css {
+                                    width = 150.px
+                                }
+                                +"08.00 - 09.00"
+                            }
+                            styledTd {
+                                css {
+                                    textAlign = TextAlign.left
+                                }
+                                +"Welcome and Photos"
+                            }
+                        }
+                        tr {
+
+                            styledTd {
+                                css {
+                                    width = 100.px
+                                }
+                                +"09.00 - 09.30"
+                            }
+                            styledTd {
+                                css {
+                                    textAlign = TextAlign.left
+                                }
+                                +"Engagement"
+                            }
+                        }
+                        tr {
+
+                            styledTd {
+                                css {
+                                    width = 100.px
+                                }
+                                +"09.30 - 11.00"
+                            }
+                            styledTd {
+                                css {
+                                    textAlign = TextAlign.left
+                                }
+                                +"Traditional Wedding Ceremony"
+                            }
+                        }
+                        tr {
+
+                            styledTd {
+                                css {
+                                    width = 100.px
+                                }
+                                +"11.00 - 13.00"
+                            }
+                            styledTd {
+                                css {
+                                    textAlign = TextAlign.left
+                                }
+                                +"Launch and Photos"
+                            }
+                        }
+                    }
+                }
+
+                styledImg(src = knot as? String) {
+                    css { width = 100.px }
+                }
+            }
+
+            styledDiv {
+                css {
+                    backgroundColor = Color.white
+                }
+                styledIframe {
+                    css {
+                        border = "0"
+                        margin(0.px)
+                        padding(0.px)
+                    }
+                    attrs {
+                        src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3751.74940046674!2d99.84863131491393!3d19.892800986624636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30d7064f17cbc839%3A0x486f6aedfce304cc!2sKong+Garden+View+Resort+Chiang+Rai!5e0!3m2!1sen!2sth!4v1536967871345"
+                        width = "100%"
+                        height = "400"
+                    }
+                }
+            }
+
+            styledDiv {
+                css {
                     padding(100.px, 0.px)
                     backgroundColor = Color("#ffffff")
                     overflow = Overflow.auto
                 }
 
                 if (state.galleryImagePublicIds.isNotEmpty()) {
-                    val cl = cloudinary.Core.new(jsObject{
+                    val cl = cloudinary.Core.new(jsObject {
                         cloudName = "ting-pop"
                     })
                     val galleryImages = state.galleryImagePublicIds.mapIndexed { index, publicId ->
